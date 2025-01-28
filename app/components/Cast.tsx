c126bf3aa7346e3fbf34808005aabd35f89ecdea
danjeljansson <Daniel.Jansson@yh.nackademin.se>
2025-01-28 17:01:51 +0100

    made the cast list tighter on desktop

--- app/components/Cast.tsx
+++ app/components/Cast.tsx
@@ -19,16 +19,16 @@ const CastList: React.FunctionComponent<CastListProps> = ({ cast: cast }) => {
   const [selectedMember, setSelectedMember] = useState<CastMember | null>(null);
 
   return (
-    <section id="ensemble" className="p-8">
+    <section id="ensemble" className="p-4">
       <h2 className="mb-8 text-center text-3xl font-bold">Ensemble</h2>
-      <ul className="grid grid-cols-1 justify-items-center gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2">
+      <ul className="grid grid-cols-1 justify-items-center gap-6 sm:grid-cols-2 sm:gap-4 md:gap-6 lg:gap-4 xl:gap-3">
         {cast.map((member) => (
           <li
             key={member.name}
-            className="flex h-[275px] w-[250px] cursor-pointer flex-col items-center justify-between overflow-hidden rounded-lg bg-white shadow-md transition-shadow duration-300 hover:shadow-xl"
+            className="flex h-[275px] w-[200px] cursor-pointer flex-col items-center justify-between overflow-hidden rounded-lg bg-white shadow-md transition-shadow duration-300 hover:shadow-xl"
             onClick={() => setSelectedMember(member)}
           >
-            <div className="relative h-32 w-32 sm:h-40 sm:w-40">
+            <div className="relative h-32 w-32 sm:h-36 sm:w-36">
               <Image
                 src={member.imageUrl}
                 alt={member.name}
