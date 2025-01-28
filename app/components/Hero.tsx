b457a90232b4a9131eb68cc131a2403f327e54e9
danjeljansson <Daniel.Jansson@yh.nackademin.se>
2025-01-28 20:59:29 +0100

    social media buttons overlay

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import Header from "@/app/components/Header";
 import BurgerMenu from "@/app/components/BurgerMenu";
 import HorizontalList from "@/app/components/ListItems";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
+import { AiFillTikTok } from "react-icons/ai";
 import Image from "next/image";
 
 const Hero = () => {
@@ -11,7 +13,6 @@ const Hero = () => {
 
   return (
     <div className="relative flex w-full flex-col">
-      {/* Hero Image */}
       <div className="relative h-[50vh] w-full sm:h-[60vh] lg:h-[80vh]">
         <Image
           src={"/images/hero.png"}
@@ -20,19 +21,45 @@ const Hero = () => {
           priority={true}
           className="object-cover"
         />
+        <div className="absolute left-1/2 top-[15%] flex -translate-x-1/2 items-center gap-8 text-white md:top-[10%] lg:top-[5%]">
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Facebook"
+            className="hover:text-blue-500"
+          >
+            <FaFacebook className="text-3xl sm:text-4xl lg:text-5xl" />
+          </a>
+          <a
+            href="https://www.tiktok.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="TikTok"
+            className="hover:text-black"
+          >
+            <AiFillTikTok className="text-3xl sm:text-4xl lg:text-5xl" />
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+            className="hover:text-pink-500"
+          >
+            <FaInstagram className="text-3xl sm:text-4xl lg:text-5xl" />
+          </a>
+        </div>
       </div>
 
-      {/* Burger Menu */}
       <BurgerMenu burgerOpen={burgerOpen} setBurgerOpen={setBurgerOpen} />
 
-      {/* Header */}
       <div className="bg-custom-green relative hidden w-full py-4 sm:block">
         <Header />
       </div>
 
-      {/* Mobile Navigation */}
       <nav
-        className={`fixed right-0 top-[30px] z-50 w-2/3 transform bg-gray-700 opacity-95 shadow-lg transition-transform duration-300 ease-in-out sm:hidden ${
+        className={`fixed right-0 top-[45px] z-50 h-full w-2/4 transform bg-gray-950 opacity-95 shadow-lg transition-transform duration-300 ease-in-out sm:hidden ${
           burgerOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
