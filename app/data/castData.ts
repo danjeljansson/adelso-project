bf2b3c550c68a24534a89155dc93bf6a3fb38fbb
danjeljansson <Daniel.Jansson@yh.nackademin.se>
2025-02-04 13:57:37 +0100

    added meta data, updated castdata

--- app/data/castData.ts
+++ app/data/castData.ts
@@ -4,39 +4,39 @@ export const castData = [
     role: "Skådespelare",
     about:
       "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc lacinia aliquet. Nullam nec nunc nec nunc. ut purus eget nunc lacinia aliquet. Nullam nec nunc nec nunclacinia aliquet. Nullam nec nunc nec nunc. ut purus eget",
-    imageUrl: "/images/cast/petra_nielsen.png",
+    imageUrl: "/images/cast/petra-nielsen.png",
   },
   {
     name: "Niki Gunke Stangertz",
     role: "Skådespelare",
     about: "Sussi is a seasoned actress ",
-    imageUrl: "/images/cast/niki_gunke.png",
+    imageUrl: "/images/cast/niki-gunke-stangertz.png",
   },
   {
     name: "Johan Charles",
     role: "Skådepelare",
     about:
       "Sussi is a seasoned actress with a long career in theater and film.",
-    imageUrl: "/images/cast/johan_charles.png",
+    imageUrl: "/images/cast/johan-charles.png",
   },
   {
     name: "Adam Wilson Wallnäs ",
     role: "Skådespelare",
     about: "",
-    imageUrl: "/images/cast/adam_wilson.png",
+    imageUrl: "/images/cast/adam-wilson-wallnas.png",
   },
   {
     name: "Ebba Irestad",
     role: "Skådespelare",
     about:
       "Sussi is a seasoned actress with a long career in theater and film.",
-    imageUrl: "/images/cast/ebba_irestad.png",
+    imageUrl: "/images/cast/ebba-irestad.png",
   },
   {
-    name: "Sussi Persson",
+    name: "Anna Åström",
     role: "Skådespelare",
     about:
       "Sussi is a seasoned actress with a long career in theater and film. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget  lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget. piscing elit. Sed ut purus eget. eben eben eben eben eben eben eben eben eben eben eben eben eben eben eben hej",
-    imageUrl: "/images/cast/sussi_larsson.png",
+    imageUrl: "/images/cast/anna-astrom.png",
   },
 ];
