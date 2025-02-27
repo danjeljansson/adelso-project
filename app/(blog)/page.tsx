3285fe017c7d7ce4c4bf2e8fd715720a5b790c8b
danjeljansson <Daniel.Jansson@yh.nackademin.se>
2025-02-27 16:21:27 +0100

    updated page and shows  for new crew component

--- app/(blog)/page.tsx
+++ app/(blog)/page.tsx
@@ -3,23 +3,26 @@ import { Posts } from "@/app/components/Posts";
 import Hero from "@/app/components/Hero";
 import About from "@/app/components/About";
 import FindUs from "@/app/components/FindUs";
-// import BuyNowButton from "@/app/components/BuyNow";
+import BuyNowButton from "@/app/components/BuyNow";
 import Shows from "@/app/components/Shows";
 import { sanityFetch } from "@sanity/lib/live";
 import { BUTTON_QUERY } from "@sanity/lib/queries";
 
 export default async function Page() {
-  const { data } = await sanityFetch({ query: BUTTON_QUERY });
-
+  const { data: buttonData } = await sanityFetch({ query: BUTTON_QUERY });
   return (
     <div className="flex flex-col justify-center">
       <Hero
         buttonData={{
-          title: data?.event ?? "",
-          hasCast: data?.hasCast,
+          title: buttonData?.event ?? "",
+          hasCast: buttonData?.hasCast,
         }}
       />
-      {/*<BuyNowButton url="https://www.kulturbiljetter.se" label="BOKA BILJETT" />*/}
+
+      {buttonData?.ticketUrl && (
+        <BuyNowButton url={buttonData.ticketUrl} label="BOKA BILJETT" />
+      )}
+
       <About />
       <Shows />
       <Posts />
--- app/components/Shows.tsx
+++ app/components/Shows.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import Cast from "@/app/components/Cast";
 import Show from "@/app/components/Show";
+import Crew from "@/app/components/Crew";
+import { CREW_QUERY } from "@sanity/lib/queries";
 import { EVENT_QUERY } from "@sanity/lib/queries";
 import { sanityFetch } from "@sanity/lib/live";
 
 const Shows = async () => {
   const { data: eventData } = await sanityFetch({ query: EVENT_QUERY });
+  const { data: crewData } = await sanityFetch({ query: CREW_QUERY });
 
   return (
     <div className="flex flex-col items-center">
       <Show eventData={eventData} />
       <Cast />
+      <Crew crewData={crewData[0]} />
     </div>
   );
 };
